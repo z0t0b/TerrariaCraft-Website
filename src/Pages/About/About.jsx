a0a06b21ff64167be4aa0d9a139cf1f3c9858ebc
z0t0b <riz00ted@gmail.com>
2021-11-28 18:18:39 -0600

    Fix for currently selected tab

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -10,7 +10,7 @@ export default function About() {
       }}
       className="w-screen h-screen bg-cover"
     >
-      <Header pageName="main" />
+      <Header pageName="about" />
       <Note />
       <div className="flex justify-center mt-2">
         <div className="flex justify-center align-middle items-center bg-black bg-opacity-60 text-white w-48 px-48 lg:w-96 lg:px-96 py-1">
